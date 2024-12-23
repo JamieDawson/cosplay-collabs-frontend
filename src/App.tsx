73732d8c45a2c5c61b60c4ba49b3ef0be93a5214
Jamie Dawson <jamierobertdawson@gmail.com>
2024-12-22 21:44:37 -0800

    Found InsagramEmbed NPM package. Going to use this for custom ad post

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { InstagramEmbed } from "react-social-media-embed";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Cosplay Collabs</h1>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <InstagramEmbed
+            url="https://www.instagram.com/p/CUbHfhpswxt/"
+            width={328}
+          />
+        </div>
       </header>
     </div>
   );
