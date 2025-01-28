eb04841cc687cf325f569bb078e437bb39acc1e1
Jamie Dawson <jamierobertdawson@gmail.com>
2025-01-27 23:03:22 -0800

    updating CSS for HomePage with Instagram post. I'm going in the right direction but I need to make sure the backgrounds don't shrink b ehind the instagram post

--- src/Components/InstagramComponent/InstagramComponent.component.tsx
+++ src/Components/InstagramComponent/InstagramComponent.component.tsx
@@ -5,9 +5,20 @@ const InstagramComponent: React.FC = () => {
     <div className="instagram-item">
       <InstagramEmbed
         url="https://www.instagram.com/p/CUbHfhpswxt/"
-        width={328}
-        height={382}
+        width={330}
+        height={380}
       />
+      <div className="instagram-item-title">
+        Batman cosplayer looking for Robin Cosplayer to team up
+      </div>
+      <div className="instagram-item-description">
+        I'm a Batman cosplayer based in CA and I'd love to team up with a
+        cosplayer who dreses as Robin!
+      </div>
+      <div className="instagram-item-tags">
+        <button>Batman</button> <button>Robin</button> <button>DC</button>{" "}
+        <button>Dark Knight</button>
+      </div>
     </div>
   );
 };
