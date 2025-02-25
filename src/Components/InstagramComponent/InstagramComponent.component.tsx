1367961a3fcf1955e20c5598364441f5eafecde7
Jamie Dawson <jamierobertdawson@gmail.com>
2025-02-24 21:41:40 -0800

    can compare database user_id to auth0 user.sub. This helps me associate users to their ads!

--- src/Components/InstagramComponent/InstagramComponent.component.tsx
+++ src/Components/InstagramComponent/InstagramComponent.component.tsx
@@ -1,4 +1,5 @@
 import { InstagramEmbed } from "react-social-media-embed";
+import { useAuth0 } from "@auth0/auth0-react";
 
 interface Ad {
   _id: string;
@@ -24,14 +25,17 @@ interface InstagramComponentProps {
 }
 
 const InstagramComponent: React.FC<InstagramComponentProps> = ({ ad }) => {
+  const { user } = useAuth0();
+  console.log(user);
+
+  console.log(ad);
   return (
     <div className="instagram-item">
+      {user?.sub === ad.user_id ? <div>USER FOUND</div> : <></>}
+
       <InstagramEmbed url={ad.instagram_post_url} />
-
       <div className="instagram-item-title">{ad.title}</div>
-
       <div className="instagram-item-description">{ad.description}</div>
-
       <div className="instagram-item-tags">
         {ad.keywords.map((keyword, index) => (
           <button key={index}>{keyword}</button>
